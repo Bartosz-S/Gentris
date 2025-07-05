a27f334f8a180a9a8059286e3bf6cc42f8a30397
Bartosz <88800970+Bartosz-S@users.noreply.github.com>
2025-07-05 21:22:50 +0200

    Player controls

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,17 +6,23 @@ public class PlayerController : MonoBehaviour
 {
     private Controller controller;
     private InputAction Moving;
-    private GameObject catchedGameObject;
     private bool hovering = false;
     [SerializeField] private Vector2 mousePosition = Vector2.zero;
-    [SerializeField] private float speed = 1f;
+    //[SerializeField] private float acceleration = 1f;
     private Ray mouseRay;
     private RaycastHit hit;
+    private Camera mainCam;
+    [SerializeField] private float maxSpeed;
+    private bool selected = false;
+    private GameObject selectedObj;
+    private Vector2 followPoint;
+    [SerializeField] private float defaultZAxis = 6f;
 
     private void Awake()
     {
         controller = new Controller();
         Moving = controller.Player.CatchObject;
+        mainCam = Camera.main;
     }
 
     private void OnEnable()
@@ -41,31 +47,47 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        mouseRay = Camera.main.ScreenPointToRay(mousePosition);
-
+        mousePosition = Mouse.current.position.ReadValue();
+        mouseRay = mainCam.ScreenPointToRay(mousePosition);
+        Debug.DrawRay(mouseRay.origin, mouseRay.direction * 10, Color.red);
+        if (selected)
+        {
+            MoveObject();
+        }
     }
 
-    private void MoveObject(GameObject selectedObject)
+    private void MoveObject()
     {
-        catchedGameObject.transform.Translate(new Vector3(mousePosition.x, mousePosition.y, selectedObject.transform.position.z)
-            * speed);
+        Physics.Raycast(mouseRay, out hit);
+        followPoint = new Vector2(hit.point.x, hit.point.y);
+        if (Mathf.Abs(selectedObj.transform.position.x - followPoint.x) > 0.01f 
+            && Mathf.Abs(selectedObj.transform.position.y - followPoint.y) > 0.01f)
+        {
+            selectedObj.transform.position = new Vector3(Vector2.Lerp(selectedObj.transform.position, followPoint, Time.deltaTime).x,
+                Vector2.Lerp(selectedObj.transform.position, followPoint, Time.deltaTime).y, defaultZAxis);
+        }
     }
 
     private void catchObject(InputAction.CallbackContext context)
     {
-
+        hovering = true;
         if (hovering)
         {
             if (Physics.Raycast(mouseRay, out hit))
             {
-                catchedGameObject = hit.collider.gameObject;
-                MoveObject(catchedGameObject);
+                selectedObj = hit.collider.gameObject;
+                //defaultZAxis = selectedObj.transform.position.z;
+                followPoint = new Vector3(hit.point.x, hit.point.y, defaultZAxis);
+                if(selectedObj.layer == LayerMask.NameToLayer("Movable")){
+                    selected = true;
+                }
             }
         }
         Debug.Log("Catched");
     }
     private void releaseObject(InputAction.CallbackContext context)
     {
+        selected = false;
         Debug.Log("Released");
     }
 
