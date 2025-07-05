f363ebf46f7ec41ad111bc80e48cca0c3dde9b08
Bartosz <88800970+Bartosz-S@users.noreply.github.com>
2025-07-05 16:27:25 +0200

    fast commit

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,11 +5,26 @@ public class PlayerController : MonoBehaviour
 {
     private Controller controller;
     private InputAction Moving;
+    private GameObject catchedGameObject;
+    private bool hovering = false;
 
     private void Awake()
     {
         controller = new Controller();
-        Moving = controller.Player.Move;
+        Moving = controller.Player.CatchObject;
+    }
+
+    private void OnEnable()
+    {
+        Moving.started += catchObject;
+        Moving.canceled += releaseObject;
+        controller.Enable();
+    }
+    private void OnDisable()
+    {
+        Moving.started -= catchObject;
+        Moving.canceled -= releaseObject;
+        controller.Disable();
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -22,5 +37,27 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void catchObject(InputAction.CallbackContext context)
+    {
+        if (hovering)
+        {
+
+        }
+        Debug.Log("Catched");
+    }
+    private void releaseObject(InputAction.CallbackContext context)
+    {
+        Debug.Log("Released");
+    }
+
+    private void OnMouseOver()
+    {
+        hovering = true;
+    }
+    private void OnMouseExit()
+    {
+        hovering = false;
     }
 }
